60dd49b1d1e78c288f359255685a6b052fe89456
John Collier <jcollier@redhat.com>
2024-11-20 12:22:19 -0500

    Fix ollama version to v0.3.14
    Signed-off-by: John Collier <jcollier@redhat.com>

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 CONTAINER_ENGINE="${VARIABLE:-podman}"
 IMAGE_REPO="${VARIABLE:-quay.io/redhat-ai-dev/ollama-ubi}"
 
-OLLAMA_VERSION=$(curl -s "https://api.github.com/repos/ollama/ollama/releases/latest" | jq -r .name)
-
+#OLLAMA_VERSION=$(curl -s "https://api.github.com/repos/ollama/ollama/releases/latest" | jq -r .name)
+OLLAMA_VERSION="v0.3.14"
 echo ${OLLAMA_VERSION} > VERSION
 
 $CONTAINER_ENGINE build --no-cache --build-arg=VERSION=${OLLAMA_VERSION} -t $IMAGE_REPO:latest -f Containerfile.build
