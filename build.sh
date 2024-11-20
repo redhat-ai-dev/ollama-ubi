5b9517e5ca07dcd7f93d4e29c08450074bd1fbbe
John Collier <jcollier@redhat.com>
2024-11-20 12:24:03 -0500

    Lock the Ollama version to v0.3.14 for now
    Signed-off-by: John Collier <jcollier@redhat.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 CONTAINER_ENGINE="${VARIABLE:-podman}"
 IMAGE_REPO="${VARIABLE:-quay.io/redhat-ai-dev/ollama-ubi}"
 
-OLLAMA_VERSION=$(curl -s "https://api.github.com/repos/ollama/ollama/releases/latest" | jq -r .name)
+#OLLAMA_VERSION=$(curl -s "https://api.github.com/repos/ollama/ollama/releases/latest" | jq -r .name)
+OLLAMA_VERSION="v0.3.14"
 
 echo ${OLLAMA_VERSION} > VERSION
 
