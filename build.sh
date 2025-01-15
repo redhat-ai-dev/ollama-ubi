e2bdd9fa056d4f488f53f7f1c7ff4bc084aa5200
John Collier <jcollier@redhat.com>
2025-01-15 15:02:04 -0500

    Upgrade ollama to v0.5.5
    Signed-off-by: John Collier <jcollier@redhat.com>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ CONTAINER_ENGINE="${VARIABLE:-podman}"
 IMAGE_REPO="${VARIABLE:-quay.io/redhat-ai-dev/ollama-ubi}"
 
 #OLLAMA_VERSION=$(curl -s "https://api.github.com/repos/ollama/ollama/releases/latest" | jq -r .name)
-OLLAMA_VERSION="v0.4.7"
+OLLAMA_VERSION="v0.5.5"
 
 echo ${OLLAMA_VERSION} > VERSION
 
