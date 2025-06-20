a052a80e7ff2ee775ce168afba00496e508cc2bd
John Collier <jcollier@redhat.com>
2025-06-20 16:06:34 -0400

    Cleanup
    Signed-off-by: John Collier <jcollier@redhat.com>

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ OLLAMA_VERSION="v0.9.1"
 
 echo ${OLLAMA_VERSION} > VERSION
 
-$CONTAINER_ENGINE build --no-cache --build-arg=OLLAMA_VERSION=${OLLAMA_VERSION} -t $IMAGE_REPO:latest -f Containerfile.build
+$CONTAINER_ENGINE build --no-cache --build-arg=OLLAMA_VERSION=${OLLAMA_VERSION} -t $IMAGE_REPO:latest -f Containerfile
 $CONTAINER_ENGINE tag $IMAGE_REPO:latest $IMAGE_REPO:${OLLAMA_VERSION}
