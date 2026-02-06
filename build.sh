94447b9acebdc30a6de728aa4a27418fef1fae1e
John Collier <jcollier@redhat.com>
2026-02-06 17:08:21 -0500

    Bump ollama-ubi to v0.15.5
    Signed-off-by: John Collier <jcollier@redhat.com>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 CONTAINER_ENGINE="${VARIABLE:-podman}"
 IMAGE_REPO="${VARIABLE:-quay.io/redhat-ai-dev/ollama-ubi}"
 
-OLLAMA_VERSION="v0.12.8"
+OLLAMA_VERSION="v0.15.5"
 
 echo ${OLLAMA_VERSION} > VERSION
 
